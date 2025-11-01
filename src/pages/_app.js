6484003d133123af1374674bbda5ba85c73bb2d9
Fabian Miranda Palma <fmir_86@hotmail.com>
2025-11-01 00:39:54 -0600

    ga4

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,10 +1,12 @@
 import '../styles/globals.scss';
 import { MainProvider } from "../context/MainContext";
+import { GoogleAnalytics } from '@next/third-parties/google';
 
 export default function App({ Component, pageProps }) {
   return (
     <MainProvider>
       <Component {...pageProps} />
+      <GoogleAnalytics gaId="G-K1B972PYHJ" />
     </MainProvider>
   );
 }
